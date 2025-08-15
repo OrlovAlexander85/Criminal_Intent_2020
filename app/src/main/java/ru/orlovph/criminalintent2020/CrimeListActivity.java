0e5d34654248e0f07f33a4ed4155ec92abc419e3
OrlovAlexander <6816106@gmail.com>
2025-08-15 16:03:28 +0200

    Update CrimeListActivity.java

--- app/src/main/java/ru/orlovph/criminalintent2020/CrimeListActivity.java
+++ app/src/main/java/ru/orlovph/criminalintent2020/CrimeListActivity.java
@@ -2,6 +2,7 @@ package ru.orlovph.criminalintent2020;
 
 import androidx.fragment.app.Fragment;
 
+@Deprecated
 public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
